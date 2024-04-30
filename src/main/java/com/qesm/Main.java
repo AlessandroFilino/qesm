ff8fe06c25dea1992204913944f75cee29fd8737
AlessandroFilino <65735750+AlessandroFilino@users.noreply.github.com>
2024-04-30 18:13:11 +0200

    partial sharedToUnsharedGraph

--- src/main/java/com/qesm/Main.java
+++ src/main/java/com/qesm/Main.java
@@ -4,11 +4,17 @@ public class Main {
     public static void main(String[] args) {
         ProductGraph graphTest = new ProductGraph();
         
-        
         graphTest.generateRandomDAG(10, 10, 3, 3);
-        graphTest.printDAG();
-        graphTest.exportDAGDotLanguage("./output/DAG.dot");
-        graphTest.renderDotFile("./output/DAG.dot", "./media/test.png", 3);
+        graphTest.sharedToUnsharedGraph(graphTest.getRootNode());
+        graphTest.printDAG(graphTest.getSharedDag());
+        
+        graphTest.exportDAGDotLanguage("./output/sharedDAG.dot", graphTest.getSharedDag());
+        //graphTest.renderDotFile("./output/sharedDAG.dot", "./media/shared.png", 3);
+
+        graphTest.exportDAGDotLanguage("./output/unSharedDAG.dot", graphTest.getUnsharedDag());
+        //graphTest.renderDotFile("./output/unSharedDAG.dot", "./media/unShared.png", 3);
+
+        
         // graphTest.importDagDotLanguage("./output/DAG.dot");
         // graphTest.exportDAGDotLanguage("./output/DAG_Test_Copy.dot");
         // System.out.println(graphTest.isDagConnected());
