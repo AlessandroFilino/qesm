6cf44698403cf30a22f01703a78219650d1892c6
AlessandroFilino <65735750+AlessandroFilino@users.noreply.github.com>
2024-07-20 19:07:57 +0200

    add test for seq to string, seq equals, composedBlock equals

--- src/test/java/com/qesm/STPNBlockTest.java
+++ src/test/java/com/qesm/STPNBlockTest.java
@@ -2,6 +2,7 @@ package com.qesm;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -43,4 +44,47 @@ public class STPNBlockTest {
         
     }
 
+    @Test
+    void testSeqToString() {
+        STPNBlock simpleBlock = new SimpleBlock(new ProductType("p0", 4, null));
+        STPNBlock simpleBlock1 = new SimpleBlock(new ProductType("p1", 4, null));
+        STPNBlock simpleBlock2 = new SimpleBlock(new ProductType("p2", 4, null));
+        STPNBlock seqBlock = new SeqBlock(new ArrayList<STPNBlock>(List.of(simpleBlock, simpleBlock1, simpleBlock2)));
+
+        String expectedString = "SeqBlock: [[p0, p1, p2]]";
+
+        assertEquals(seqBlock.toString(), expectedString);
+    }
+
+    @Test
+    void testSeqEquals(){
+        STPNBlock simpleBlock0 = new SimpleBlock(new ProductType("p0", 4, null));
+        STPNBlock simpleBlock1 = new SimpleBlock(new ProductType("p1", 4, null));
+        
+        STPNBlock seqBlock0 = new SeqBlock(new ArrayList<STPNBlock>(List.of(simpleBlock0, simpleBlock1)));
+        STPNBlock seqBlock_0 = new SeqBlock(new ArrayList<STPNBlock>(List.of(simpleBlock0, simpleBlock1)));
+
+        assertEquals(seqBlock0, seqBlock_0);
+
+    }
+
+    @Test 
+    void testEquals(){
+        STPNBlock simpleBlock = new SimpleBlock(new ProductType("p0", 4, null));
+        STPNBlock simpleBlock1 = new SimpleBlock(new ProductType("p1", 4, null));
+        STPNBlock andBlock = new AndBlock(new ArrayList<STPNBlock>(List.of(simpleBlock, simpleBlock1)));
+        
+        STPNBlock andBlock1 = new AndBlock(new ArrayList<STPNBlock>(List.of(simpleBlock, simpleBlock1)));
+        assertNotEquals(andBlock, null);
+        assertEquals(andBlock, andBlock1);
+        assertEquals(andBlock1, andBlock);
+        
+
+        STPNBlock simpleBlock2 = new SimpleBlock(new ProductType("p2", 4, null));
+        STPNBlock andBlock2 = new AndBlock(new ArrayList<STPNBlock>(List.of(simpleBlock, simpleBlock1, simpleBlock2)));
+        assertNotEquals(andBlock2, andBlock1);
+        assertNotEquals(andBlock1, andBlock2);
+        
+    }
+    
 }
